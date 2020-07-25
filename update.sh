be5cc0952a088ac060ee0c097236c7ab55b4c365
Yiyang <yyhsu@protonmail.com>
2020-07-25 21:01:17 +0800

    README update

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ find content/ -name "*.md" -exec bash -c 'opencc -i "$1" -o "${1%.md}".zht.md -c
 cd static/txt/
 find . -maxdepth 1 -name "*.txt" -exec bash -c 'opencc -i "$1" -o zht/`echo ${1%.txt} | opencc -c s2twp.json`.txt -c s2twp.json' - '{}' \;
 cd ../../
-/usr/bin/env hugo -D -d shota
+/usr/bin/env hugo -D -d public
